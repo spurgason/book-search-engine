a849026fabbcd27b92e9823396d45edeeca546d2
spurgason <shanepurgason.98@gmail.com>
2021-12-16 19:05:49 -0500

    added apollo to server, and auth to work with graphql

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { start } = require('repl');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefsDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  await server.start();
+
+  server.applyMiddleware({app});
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
